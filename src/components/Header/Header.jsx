f64f27811a57d310a99a8b3e997bc03a910ef99c
jesandante007 <jesandante007@gmail.com>
2023-03-31 05:45:46 +0600

    header section done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from '../../assets/profile.png'
+
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center py-5 border-b-2'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={img} alt="profile image" className='w-14 h-14 rounded-full' />
+        </header>
+    );
+};
+
+export default Header;
