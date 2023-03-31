6dc7ed6703c8baf7844d92ce6c67308fb71367f2
jesandante007 <jesandante007@gmail.com>
2023-03-31 09:27:30 +0600

    sideCart positioning issue fixed

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideCart = ({time, titles}) => {
     return (
-        <div className='md:sticky md:top-0'>
+        <div className='md:h-screen md:sticky md:top-0'>
             <div className='text-center py-5 rounded-lg bg-purple-100 text-purple-700 text-2xl font-bold mb-6'>Spent time on read: {time} min</div>
             <div className='bg-gray-100 p-7 rounded-lg'>
                 <h3 className='text-2xl font-bold mb-4'>Bookmarked Blogs: {titles.length}</h3>
