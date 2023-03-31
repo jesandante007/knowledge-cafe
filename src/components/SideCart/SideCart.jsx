a15b6b3bb857faef13a5a7105bb58f8d4821c8b6
jesandante007 <jesandante007@gmail.com>
2023-03-31 09:10:06 +0600

    Bookmark count added & exo fontfamily applied

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -5,7 +5,7 @@ const SideCart = ({time, titles}) => {
         <div className='md:sticky md:top-0'>
             <div className='text-center py-5 rounded-lg bg-purple-100 text-purple-700 text-2xl font-bold mb-6'>Spent time on read: {time} min</div>
             <div className='bg-gray-100 p-7 rounded-lg'>
-                <h3 className='text-2xl font-bold mb-4'>Bookmarked Blogs: {0}</h3>
+                <h3 className='text-2xl font-bold mb-4'>Bookmarked Blogs: {titles.length}</h3>
                 {
                     titles.map((title, index) => <div key={index} className='bg-white rounded-lg text-lg font-semibold p-5 mb-4'>
                         {title}
